92b422612b7f3af17ac7c44b378bda0949f6321f
Maite Gracia <maitegracia223@gmail.com>
2016-11-29 14:51:53 +0000

    Header + menu + intro

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,15 @@
 
 // Hide loader after 5 secs
-$('.full-page-loader').delay(5000).fadeOut('slow');
+$('.full-page-loader').delay(6000).fadeOut('slow');
+
+
+//
+// var paths = $('path:not(defs path)');
+// paths.each(function(i, e) {
+//     e.style.strokeDasharray = e.style.strokeDashoffset = e.getTotalLength();
+// });
+// var tl = new TimelineMax();
+// tl.add([
+//     TweenLite.to(paths.eq(0), 1, {strokeDashoffset: 0, delay: 0.0}),
+//     TweenLite.to(paths.eq(1), 1, {strokeDashoffset: 0, delay: 0.5})
+// ]);
